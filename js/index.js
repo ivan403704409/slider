4b0b8aafaf98c6f786371d963a15c5a10425e224
ivan403704409 <403704409@qq.com>
2014-10-09 22:55:16 +0800

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,48 @@
+function Slider(){
+  
+  this.lastIndex = 0;
+  this.curIndex = 0;
+}
+
+Slider.prototype = {
+  
+  init: function (){
+    var self = this;
+    
+    self._bind();
+    
+  },
+  
+  _bind: function (){
+    
+    
+    
+  },
+  
+  _switch: function (){
+    
+    
+    
+  },
+  
+  _startAutoPlay: function (){
+    
+    
+  },
+  
+  _stopAutoPlay: fuction (){
+    
+    
+  },
+  
+  _isGoLeft : function (){
+    
+    
+  },
+  
+  
+  
+  
+    
+  constructor: Slider
+};
